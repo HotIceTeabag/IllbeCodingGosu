8524e8642b9270d370b0c6989ac16c06aa8c3748
HotIceTea <105905146+HotIceTeabag@users.noreply.github.com>
2024-10-10 17:04:10 +0900

    code 업데이트

--- temp/code.c
+++ temp/code.c
@@ -7,7 +7,6 @@ ex)버블소트, 행렬곱, 문자열 비교 등
 
 
 //버블소트 함수
-
 for (int i = 0; i < len - 1; i++) {
 	for (int j = 0; j < len - (i + 1); j++) {
 		if (arr[j] > arr[j + 1]){
@@ -22,7 +21,19 @@ for (int i = 0; i < len - 1; i++) {
 //원소 위치찾기 함수
 int loc(const int* arr, const int number, const int len) {
 	for (int i = 0; i < len; i++)if (arr[i] == number)return i;
-	return 0;
+	return -1;
+}
+
+//배열 복사함수
+(인티저)
+void cpy(const int* oriarr, int* newarr, const int len)for (int i = 0; i < len; i++) *newarr + i = *oriarr + i;
+
+//제곱함수
+int square(const int number, const int count) {
+	if (count == 0)return 1;
+	int value = number;
+	for (int i = 1; i < count; i++)value *= number;
+	return value;
 }
 
 //문자열 비교 함수
